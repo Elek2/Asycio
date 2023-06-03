c6a5555574fc6c680a3edad8a21fa63812bd0a1e
Elek <Elek2@yandex.ru>
2023-06-03 14:47:46 +0300

    async initital

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
+import asyncio
 import requests
-from models import StarPeople
+import aiohttp
 
-from models import Session
-
-
-def person_get(pers_id):
-    response = requests.get(f"https://swapi.dev/api/people/{pers_id}")
-    return response.json()
+from models import Session, StarPeople
 
 
-def link_name_convert(links: list) -> list:
-    names_list = []
-    for link in links:
-        response = requests.get(link)
-        name = response.json().get('name')
-        if not name:
-            name = response.json().get('title')
-        names_list.append(name)
-    return names_list
+async def person_get(pers_id):
+    session = aiohttp.ClientSession()
+    response = await session.get(f"https://swapi.dev/api/people/{pers_id}")
+    await session.close()
+    return await response.json()
 
 
-def put_into_db(pers_id):
+# async def link_name_convert(links: list) -> list:
+#     names_list = []
+#     for link in links:
+#         response = requests.get(link)
+#         name = response.json().get('name')
+#         if not name:
+#             name = response.json().get('title')
+#         names_list.append(name)
+#     return names_list
+
+
+async def put_into_db(pers_id):
     with Session() as s:
         table_columns = StarPeople.__table__.columns.keys()
-        person = person_get(pers_id)
+        person = await person_get(pers_id)
         excluded_fields = set(person).difference(set(table_columns))
         for field in excluded_fields:
             person.pop(field)
 
-        for parameter in person:
-            if isinstance(person.get(parameter), list):
-                person[parameter] = link_name_convert(person.get(parameter))
+        # for parameter in person:
+        #     if isinstance(person.get(parameter), list):
+        #         person[parameter] = link_name_convert(person.get(parameter))
 
         new_person = StarPeople(**person)
         s.add(new_person)
@@ -41,4 +44,5 @@ def put_into_db(pers_id):
 
 
 if __name__ == '__main__':
-    put_into_db(1)
+    asyncio.run(put_into_db(1))
+    # put_into_db(1)
